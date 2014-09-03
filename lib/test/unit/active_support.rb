993c5b89af54c0369f74f00368b97499f04ea948
yoshihara <h.yoshihara@everyleaf.com>
2014-09-03 21:17:41 +0900

    Support Rails 4 (drop to support Rails 3.2.16 or older)
    I can check it works well with simple Rails test only.
    I use it with rails 4.1.4 so if I find bugs, I'll fix them ASAP...

--- lib/test/unit/active_support.rb
+++ lib/test/unit/active_support.rb
@@ -16,74 +16,12 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
-require "test/unit/active_support/version"
-
 require "test-unit"
-require "active_support/testing/setup_and_teardown"
+require "test/unit/assertion-failed-error"
 
-module ActiveSupport::Testing::SetupAndTeardown
-  if const_defined?(:ClassMethods)
-    module ClassMethods
-      remove_method :setup
-      remove_method :teardown
-    end
-  end
-
-  if const_defined?(:ForClassicTestUnit)
-    module ForClassicTestUnit
-      remove_method :run
-    end
-  end
-end
-
-unless ActiveSupport::Testing::SetupAndTeardown.const_defined?(:ForClassicTestUnit)
-  module MiniTest
-    Assertion = Test::Unit::Assertions
-  end
-end
-
-require 'active_support/test_case'
-
-class Test::Unit::Error
-  if method_defined?(:message_without_silenced_deprecation)
-    undef_method :message
-    alias_method :message, :message_without_silenced_deprecation
-  end
-end
-
-class ActiveSupport::TestCase
-  def valid?
-    return false if @method_name == "default_test"
-    super
-  end
-end
-
-class Test::Unit::TestCase
-  # REMOVE ME after https://github.com/freerange/mocha/pull/89 is merged.
-  if method_defined?(:run_before_mocha) and not method_defined?(:cleanup_mocha)
-    alias_method :run, :run_before_mocha
-    remove_method :run_before_mocha
-
-    exception_handler(:handle_mocha_expectation_error)
-
-    cleanup :cleanup_mocha, :after => :append
-    teardown :teardown_mocha, :after => :append
-
-    private
-    def cleanup_mocha
-      assertion_counter = Mocha::Integration::TestUnit::AssertionCounter.new(current_result)
-      mocha_verify(assertion_counter)
-    end
-
-    def teardown_mocha
-      mocha_teardown
-    end
-
-    def handle_mocha_expectation_error(exception)
-      return false unless exception.is_a?(Mocha::ExpectationError)
-      problem_occurred
-      add_failure(exception.message, exception.backtrace)
-      true
-    end
+module ActiveSupport
+  remove_const :TestCase
+  class TestCase < ::Test::Unit::TestCase
+    Assertion = ::Test::Unit::AssertionFailedError
   end
 end
