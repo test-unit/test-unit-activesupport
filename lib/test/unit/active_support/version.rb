524eeb4101737e26fac67d1abe07c37fe56ed55a
Kouhei Sutou <kou@clear-code.com>
2015-08-28 23:11:36 +0900

    Bump version

--- lib/test/unit/active_support/version.rb
+++ lib/test/unit/active_support/version.rb
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2012  Kouhei Sutou <kou@clear-code.com>
+# Copyright (C) 2012-2015  Kouhei Sutou <kou@clear-code.com>
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -19,7 +19,7 @@
 module Test
   module Unit
     module ActiveSupport
-      VERSION = "1.0.4"
+      VERSION = "1.0.5"
     end
   end
 end
