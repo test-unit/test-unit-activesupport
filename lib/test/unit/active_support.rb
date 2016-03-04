e5466098a0ba61b9e63efec6b19b669da29a1c09
Kouhei Sutou <kou@clear-code.com>
2016-03-04 23:52:29 +0900

    Fix style

--- lib/test/unit/active_support.rb
+++ lib/test/unit/active_support.rb
@@ -44,7 +44,7 @@ module ActiveSupport
 
     # rails 4.1 (action dispatch assertions) needs the 'message'
     # method which is not defined in test-unit 3
-    def message msg = nil, ending = nil, &default
+    def message(msg=nil, ending=nil, &default)
       proc do
         msg = msg.call.chomp(".") if Proc === msg
         custom_message = "#{msg}.\n" unless msg.nil? or msg.to_s.empty?
