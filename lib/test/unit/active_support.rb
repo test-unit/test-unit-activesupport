0a3b0f394eec036b3a961bf4636f45744b86612a
Kouhei Sutou <kou@clear-code.com>
2016-03-04 23:51:59 +0900

    Use do/end

--- lib/test/unit/active_support.rb
+++ lib/test/unit/active_support.rb
@@ -45,11 +45,11 @@ module ActiveSupport
     # rails 4.1 (action dispatch assertions) needs the 'message'
     # method which is not defined in test-unit 3
     def message msg = nil, ending = nil, &default
-      proc {
+      proc do
         msg = msg.call.chomp(".") if Proc === msg
         custom_message = "#{msg}.\n" unless msg.nil? or msg.to_s.empty?
         "#{custom_message}#{default.call}#{ending || "."}"
-      }
+      end
     end
 
     private
